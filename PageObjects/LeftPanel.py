9922c2533112c7343e068fbb6760da9324b72729
katherine <katya.pereverzeva2109@gmail.com>
2020-01-02 17:51:47 +0200

    describe server_database page

--- PageObjects/LeftPanel.py
+++ PageObjects/LeftPanel.py
@@ -5,7 +5,8 @@ from PageObjects.BasePage import BasePage
 
 class LeftPanelLocators:
     databases_list_left_panel = (By.XPATH, "//li[@class = 'database' or @class = 'last database']/a")
-
+    add_database_button = (By.CSS_SELECTOR, "#pma_navigation_tree_content a.hover_show_full")
+    selected_db = (By.CSS_SELECTOR, "li.database.selected")
 
 class LeftPanelTestData:
     db_names = ['information_schema', 'mysql', 'performance_schema', 'phpmyadmin', 'test']
@@ -15,3 +16,12 @@ class LeftPanel(BasePage):
 
     def get_list_of_databases(self):
         return [i.text for i in self.find_elements(LeftPanelLocators.databases_list_left_panel)]
+
+    def click_add_database(self):
+        return self.find_element(LeftPanelLocators.add_database_button).click()
+
+    def get_name_of_selected_database(self):
+        return self.find_element(LeftPanelLocators.selected_db).text
+
+
+
---
+++ PageObjects/ServerDatabasePage.py
@@ -0,0 +1,17 @@
+from PageObjects.BasePage import BasePage
+from selenium.webdriver.common.by import By
+
+
+class ServerDatabaseLocators:
+    db_name_input = (By.CSS_SELECTOR, "#text_create_db")
+    db_submit_button = (By.CSS_SELECTOR, "#buttonGo")
+
+
+class ServerDatabasePage(BasePage):
+
+    def type_db_name(self, word):
+        name_input = self.find_element(ServerDatabaseLocators.db_name_input)
+        name_input.send_keys(word)
+
+    def click_submit(self):
+        return self.find_element(ServerDatabaseLocators.db_submit_button).click()
