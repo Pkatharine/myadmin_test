9dafa41071d802b6c12279a54fba751e2a5c80a2
Koreiba Roman <koreibaroman@gmail.com>
2020-01-08 17:33:46 +0200

    qwe

--- Tests/test_query_db_create.py
+++ Tests/test_query_db_create.py
@@ -5,6 +5,6 @@ from PageObjects.SQLPage import SQLPage
 
 def test_default_databases(browser):
     sql_page = SQLPage(browser)
-    time.sleep(5)
-    sql_page.click_element('#title="SQL"')
+    # sql_page.go_to_site()
+    sql_page.click_element('[title="SQL"]')
     sql_page.createDBQuery('CREATE DATABASE `test_table`')
