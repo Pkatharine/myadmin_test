aff93d84bd411b890b8c61f840d2c8f8a2335f6e
katherine <katya.pereverzeva2109@gmail.com>
2020-01-19 13:40:26 +0200

    add allure dependency

--- Tests/test_default_databases.py
+++ Tests/test_default_databases.py
@@ -1,8 +1,13 @@
+import allure
 from page_objects.left_navigation_page import LeftPanelTestData
 
 
+@allure.suite('Verify that default databases are present on home page')
+@allure.severity(allure.severity_level.MINOR)
+@allure.link("http://localhost/phpmyadmin/index.php?lang=en", name='PhpMyAdmin')
 def test_default_databases(app):
     actual = app.left_panel.get_list_of_databases()
     expected = LeftPanelTestData.db_names
-    for i in expected:
-        assert i in actual
+    with allure.step("Verify list of default db's"):
+        for i in expected:
+            assert i in actual
